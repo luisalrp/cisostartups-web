dd9d6ab08a62b2304739ff84eec4a188f12b3ff6
luisalrp <31384138+luisalrp@users.noreply.github.com>
2022-04-25 13:31:57 +0200

    Delete useSubscribeToNewsletter.ts

--- useSubscribeToNewsletter.ts
+++
@@ -1,9 +0,0 @@
-import { Form, FormState } from '@pages/api/v1/suscribe';
-import { useRef, useState } from 'react';
-
-export function useSubscribeToNewsletter() {
-	const [form, setForm] = useState<FormState>({ state: Form.Initial });
-	const inputEl = useRef(null);
-
-
-}
